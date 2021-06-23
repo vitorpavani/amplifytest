9547f6dbce74a3f2abcf90e7d20ba5b112a7692b
Vitor Pavan <vipavani@hotmail.com>
2021-06-23 16:05:16 +0200

    finally

--- amplify/backend/function/amplifytest04fcd0ba/src/app.js
+++ amplify/backend/function/amplifytest04fcd0ba/src/app.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
 app.get('/items', function (req, res) {
   // Add your code here
   res.json({
-    success: 'get call succeed BUILD BACKEND MOTHERFUCKER!basdasdj!!',
+    success: 'get call succeed!!!',
     url: req.url,
   });
 });
