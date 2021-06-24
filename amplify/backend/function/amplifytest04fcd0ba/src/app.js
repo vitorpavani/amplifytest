0b1740365ec8dd5d9cc2b296494052cd2b9872b7
Vitor Pavan <vipavani@hotmail.com>
2021-06-24 10:28:10 +0200

    test 01

--- amplify/backend/function/amplifytest04fcd0ba/src/app.js
+++ amplify/backend/function/amplifytest04fcd0ba/src/app.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
 app.get('/items', function (req, res) {
   // Add your code here
   res.json({
-    success: 'get call succeedasldasldjsadlkdwdwfdefdfjsaljdsa!!!',
+    success: 'get call test 01!!!',
     url: req.url,
   });
 });
